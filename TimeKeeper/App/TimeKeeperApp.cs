532fc5a3643232b0cac6cf71338f67edc319b0b0
Stephanie Hvenegaard <spehe@vestas.com>
2025-10-01 10:47:34 +0200

    Incrment version number

--- TimeKeeper/App/TimeKeeperApp.cs
+++ TimeKeeper/App/TimeKeeperApp.cs
@@ -32,7 +32,7 @@ namespace TimeKeeper.App
     bool isRunning = true;
     int ActiveProjectId = 0;
 
-    string version = "1.1.2";
+    string version = "1.1.3";
 
     public CalendarManager Calendar { get; private set; }
     public CalendarSettings Project { get; private set; }
